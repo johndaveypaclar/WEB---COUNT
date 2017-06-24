1ef980cc2b9069926cbd223ee667c81cd3e5912f
johndaveypaclar <johndavepaclar@gmail.com>
2017-06-24 16:04:49 +0800

    Add files via upload

---
+++ WEB - COUNT TEXT TWIST.py
@@ -0,0 +1,117 @@
+
+point =0 
+print("From the word BREAKER")
+
+breaker={"ark":1,
+         "eke":2,
+         "err":3,
+         "era":4,
+         "bee":5,
+         "rare":6,
+         "reek":7,
+         "bake":8,
+         "bark":9,
+         "bare":10,
+         "beer":11,
+         "beak":12,
+         "bear":13,
+         "baker":14,
+         "brake":15,
+         "break":16,
+         "barker":17,
+         "beaker":18,
+         "bearer":19,
+         "breaker":20,
+         "are":21,
+         "ear":22,
+         "ere":23,
+         "bar":24,
+         "bra":25,
+         "rake":26,
+         "rear":27
+         }
+
+inp= input("Input 1:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 2:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 3:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 4:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 5:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 6:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 7:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 8:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 9:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 10:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 11:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 12:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 13:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 14:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 15:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 16:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 17:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 18:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 19:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 20:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 21:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 22:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 23:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 24:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 25:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 26:")
+if inp in breaker:
+    point= point+1
+inp= input("Input 27:")
+if inp in breaker:
+    point= point+1
+
+print("YOU GOT ",point,"OUT OF 27")
+
